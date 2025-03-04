241c05fe42e9482d33fa2588f8066a7d447bee2d
Kellen Betts <kellen.betts@gmail.com>
2025-03-04 11:27:52 -0800

    initializing repository

---
+++ config.py
@@ -0,0 +1,80 @@
+########  GENERAL APP INFORMATION  ##############
+
+APP_TITLE = None
+# APP_TITLE = "SC4x | Week 10 | Case Study | ML 2"
+
+APP_INTRO = None
+# APP_INTRO = """This app..."""
+
+APP_HOW_IT_WORKS = None
+# APP_HOW_IT_WORKS = """ """
+
+SHARED_ASSET = {}
+# SHARED_ASSET = {
+#     "name":"NAME",
+#     "path":"FILE.pdf",
+#     "button_text":"Read this PDF first"
+# }
+
+HTML_BUTTON = {}
+
+COMPLETION_MESSAGE = "Thank you for submitting a response!"
+COMPLETION_CELEBRATION = False
+
+SCORING_DEBUG_MODE = False
+
+ ####### PHASES INFORMATION #########
+
+PHASES = {
+    "attempt1": {
+        "type": "text_area",
+        "height": 200,
+        "label": """Question 1: Which classification algorithms would you use?""",
+        "instructions": """ The students are using machine learning for a classification problem. They have only learned about these specific algorithms/models: tree models, random forest, Naïve Bayes, and K-nearest neighbor (kNN). They are asked to briefly describe which machine learning algorithm/models they would use. Evaluate their response and provide feedback about the algorithms/models they propose within the context of the specific algorithms/models they have learned (described above). """,
+        "value": " ",
+        "scored_phase": False,
+        "rubric": """
+            None
+        """,
+        "minimum_score": 2
+    },
+}
+
+######## AI CONFIGURATION #############
+
+########## AI ASSISTANT CONFIGURATION #######
+ASSISTANT_NAME = "testing_v1_1"
+ASSISTANT_INSTRUCTIONS = """ You are an expert in machine learning and overseeing a course where students are learning the basics of machine learning. """
+
+LLM_CONFIGURATION = {
+    "gpt-4-turbo":{
+        "name":ASSISTANT_NAME,
+        "instructions": ASSISTANT_INSTRUCTIONS,
+        "tools":[{"type":"file_search"}],
+        "model":"gpt-4-turbo",
+        "temperature":0,
+        "price_per_1k_prompt_tokens":.01,
+        "price_per_1k_completion_tokens": .03
+    },
+    "gpt-4o":{
+        "name":ASSISTANT_NAME,
+        "instructions": ASSISTANT_INSTRUCTIONS,
+        "tools":[{"type":"file_search"}],
+        "model":"gpt-4o",
+        "temperature":0,
+        "price_per_1k_prompt_tokens":.0025,
+        "price_per_1k_completion_tokens": .010
+    },
+    "gpt-3.5-turbo":{
+        "name":ASSISTANT_NAME,
+        "instructions": ASSISTANT_INSTRUCTIONS,
+        "tools":[{"type":"file_search"}],
+        "model":"gpt-3.5-turbo-0125",
+        "temperature":0,
+        "price_per_1k_prompt_tokens":0.0005,
+        "price_per_1k_completion_tokens": 0.0015
+    }
+}
+
+ASSISTANT_THREAD = ""
+ASSISTANT_ID_FILE = "assistant_id.txt"
